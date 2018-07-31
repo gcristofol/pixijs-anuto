d3366226ad384a77f8319ae90d5be2e7731c0db5
Gerard Cristofol <gerard.cristofol@vista.co>
2018-07-31 21:44:32 +1200

    POC out of bounds

--- src/wave/Wave.js
+++ src/wave/Wave.js
@@ -1,4 +1,4 @@
-const ENEMY_VELOCITY = 0.9;
+const ENEMY_VELOCITY = 4;
 
 class Wave {
 
@@ -8,18 +8,12 @@ class Wave {
   //enemy;
 
   constructor(waveScene, mWaveNumber) {
+    this.b = new Bump(PIXI);
     this.mWaveNumber = mWaveNumber;
     this.enemy = null;
 
     //this.enemy = new Sprite(resources["images/cat.png"].texture);
     this.enemy = new Enemy(TILE_SIZE * 2 + 1, 0 + 1)
-
-    //Initial Tile
-    //this.enemy.x = TILE_SIZE * 2 + 1
-    //this.enemy.y = 0 + 1
-
-    //this.enemy.scale.x = .35 //TODO remove when there are proper textures
-    //this.enemy.scale.y = .35
 
     //move down by default until first collision
     this.enemy.vy = ENEMY_VELOCITY
@@ -33,59 +27,27 @@ class Wave {
   }
 
   move(map) {
-    //move the enemy
+    //TODO limit next move to distance to wall
+
+    //move the enemy(s)
     this.enemy.y += this.enemy.vy
     this.enemy.x += this.enemy.vx
-    
-    //TODO check enemy makes it outside the map
 
-    //check if the next move has crossed boundaries
-    if (this.contain(this.enemy, map)) {
-      console.log("enemy hits wall");
-      //determine where to go next
-      var col = Math.floor(this.enemy.x / TILE_SIZE);
-      var row = Math.floor(this.enemy.y / TILE_SIZE);
-      console.log("where to go next...", map[row][col]);
-      //TODO do something in case 'F' value (the end is reached)
-      switch (map[row][col]) {
-        case 'R':
-          this.enemy.vy = 0
-          this.enemy.vx = ENEMY_VELOCITY
-          break;
-        case 'D':
-          this.enemy.vy = ENEMY_VELOCITY
-          this.enemy.vx = 0
-          break;
-        case 'L':
-          this.enemy.vy = 0
-          this.enemy.vx = -ENEMY_VELOCITY
-          break;
-        case 'U':
-          this.enemy.vy = -ENEMY_VELOCITY
-          this.enemy.vx = 0
-          break;
-      }
+    //check if the move is out of bounds
+    var col = Math.floor(this.enemy.x / TILE_SIZE);
+    var row = Math.floor(this.enemy.y / TILE_SIZE);
+
+    if (typeof map[row] != 'undefined' && typeof map[row][col] != 'undefined' && map[row][col]==='F'){
+      console.log("enemy reaches the end"); 
+      //TODO remove enemy update lives...
+      this.enemy.visible = false
+      this.enemy = null
     }
-  }
-
-  contain(sprite, map) {
-    //if sprite moving down
-    var col, row
-    if (sprite.vy > 0) {
-      col = Math.floor(sprite.x / TILE_SIZE);
-      row = Math.floor((sprite.y + sprite.height) / TILE_SIZE);
-    } else if (sprite.vx > 0) {
-      col = Math.floor((sprite.x + sprite.width) / TILE_SIZE);
-      row = Math.floor(sprite.y / TILE_SIZE);
-    } else if (sprite.vx < 0) {
-      col = Math.floor((sprite.x - 1) / TILE_SIZE);
-      row = Math.floor(sprite.y / TILE_SIZE);
-    }else if (sprite.vy < 0) {
-      col = Math.floor(sprite.x / TILE_SIZE);
-      row = Math.floor((sprite.y - 1) / TILE_SIZE);
+    else if (this.enemy.isContained(map)) {
+      console.log("enemy hits a wall");
+      //determine where to go next by reading the map
+      this.enemy.turnTo(map[row][col]);
     }
-
-    return map[row][col] === '0'
   }
   
   head() {
@@ -93,26 +55,4 @@ class Wave {
     return this.enemy;
   }
 
-}
-
-class Enemy extends PIXI.Container{
-  constructor(x, y) {
-    super()
-    this.position.x = x
-    this.position.y = y
-
-    var childSprite = new Sprite(resources["images/cat.png"].texture);
-    childSprite.scale.x = .35
-    childSprite.scale.y = .35
-    
-    childSprite.position.x = 5
-    childSprite.position.y = 5
-
-    var rectangle = new PIXI.Graphics();
-    rectangle.lineStyle(1, 0xFF5733, 3);
-    rectangle.drawRect(0, 0, TILE_SIZE, TILE_SIZE);
- 
-    this.addChild(rectangle)
-    this.addChild(childSprite)
-  }
 }
